a2123c1027a158dd3b4c9945b51d33c73da59306
Anolay900 <andalrom@gmail.com>
2021-09-17 18:56:32 +0300

    input+DOM

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,32 @@
 function sum() {
-    x = prompt("Первое слагаемое")
-    y = prompt("Второе слагаемое")
-    alert(`${x} + ${y} = ${Number(x) + Number(y)}`)
+    let x = document.getElementById('numberOne').value;
+    let y = document.getElementById('numberTwo').value;
+    let effect = document.getElementById('outcome');
+    effect.textContent = (`${x} + ${y} = ${Number(x) + Number(y)}`);
 }
 
 function sub() {
-    x = prompt("Уменьшаемое")
-    y = prompt("Вычитаемое")
-    alert(`${x} - ${y} = ${Number(x) - Number(y)}`)
+    let x = document.getElementById('numberOne').value;
+    let y = document.getElementById('numberTwo').value;
+    let effect = document.getElementById('outcome');
+    effect.textContent = (`${x} - ${y} = ${Number(x) - Number(y)}`);
 }
 
 function mult() {
-    x = prompt("Множимое")
-    y = prompt("Множитель")
-    alert(`${x} * ${y} = ${Number(x) * Number(y)}`)
+    let x = document.getElementById('numberOne').value;
+    let y = document.getElementById('numberTwo').value;
+    let effect = document.getElementById('outcome');
+    effect.textContent = (`${x} * ${y} = ${Number(x) * Number(y)}`);
 }
 
 function division() {
-    x = prompt("Делимое")
-    y = prompt("Делитель")
-    alert(`${x} / ${y} = ${Number(x) / Number(y)}`)
+    let x = document.getElementById('numberOne').value;
+    let y = document.getElementById('numberTwo').value;
+    let effect = document.getElementById('outcome');
+    effect.textContent = (`${x} / ${y} = ${Number(x) / Number(y)}`);
+}
+
+function cleaning() {
+    let effect = document.getElementById('outcome');
+    effect.textContent = '';
 }
